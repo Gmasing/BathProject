2a6fc7a22e3675d8de84aef6b42f5149a2b2b285
Gmasing <giorgiolauriola789@gmail.com>
2024-04-21 19:58:19 +0100

    Update Form1.cs

--- Bath_Revision_Project/Form1.cs
+++ Bath_Revision_Project/Form1.cs
@@ -48,11 +48,7 @@ namespace Bath_Revision_Project
         private void button2_Click(object sender, EventArgs e)
         {
             string Task = InputBox.Text;
-            /*ToDoList.Text
-             
-             testing if git works
-            testing
-             */
+            /*ToDoList.Text*/
         }
 
         private void InputBox_TextChanged(object sender, EventArgs e)
